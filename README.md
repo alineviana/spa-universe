8a36372dbd6b69734c8e889d66591f3fa08047a3
Aline Cristina Viana da Costa <alinecristinaviana@gmail.com>
2022-12-16 16:09:27 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# spa-universe
-Conceitos de SPA | Programa Explorer da Rocketseat
+# Projeto 10 | Programa Explorer da Rocketseat
+
+<br>
+
+<img src="./assets/preview.png" width=450>
+
+<br>
+
+> ## 📝 Conceitos estudados
+- Conceitos de SPA;
+- Mapeamento de rotas;
+- Assíncrono e promises;
+- Orientação a objetos;
+- Classes;
+- Responsividade.
+
+<br>
+
+> ## 🖥️ Tecnologias
+<br>
+<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" width=50px/>
+<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" width=50px/>
+<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" width=50px/> 
---
+++ js/router.js
@@ -0,0 +1,58 @@
+export default class Router {
+
+    routes = {}
+
+    add(routeName, page) {
+        this.routes[routeName] = page
+    }
+
+    route(event) {
+        event = event || window.event
+        event.preventDefault()
+
+        window.history.pushState({}, "", event.target.href)
+
+        this.handle()
+    }
+
+    changeBackground() {
+        const { pathname } = window.location;
+      
+        const { body } = document;
+      
+        switch (pathname) {
+          case '/':
+            body.className = 'home';
+            break;
+      
+          case '/index.html':
+            body.className = 'home';
+            break;
+        
+          case '/universe':
+            body.className = 'universe';
+            break;
+      
+          case '/explorer':
+            body.className = 'explorer';
+            break;
+      
+          default:
+            body.className = '';
+            break;
+        }
+    }   
+
+    handle() {
+        const { pathname } = window.location
+        const route = this.routes[pathname] || this.routes[404]
+        this.changeBackground()
+
+        fetch(route)
+        .then((data) => data.text())
+        .then((html) => {
+            document.querySelector('#app').innerHTML = html
+        })
+    }
+
+}
